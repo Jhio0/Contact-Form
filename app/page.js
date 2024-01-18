aa0376265438b75d128d27966813b1791f2599b9
Jhio0 <120062814+Jhio0@users.noreply.github.com>
2024-01-18 10:10:47 -0700

    Initla Front-end Mockup

---
+++ app/page.js
@@ -0,0 +1,14 @@
+"use client"
+import React from "react";
+import Contact from "./components/Contact";
+
+const Page = () => {
+  return (
+    <div className="w-full h-auto mx-auto max-w-7xl py-10"> 
+      <Contact/>
+    </div>
+
+  );
+};
+
+export default Page;
